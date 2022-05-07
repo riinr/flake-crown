f40262cad206461728c8f3252b02f68d50258dcb
hugosenari <hugosenari@gmail.com>
2022-05-07 07:36:26 -0300

    chore(stats): publishing some stats

--- refresher/stats.nix
+++ refresher/stats.nix
@@ -1,4 +1,8 @@
 {
+  files.alias.binary-list=
+    ''
+      jq -r 'select(.binDir != "" and .binDir != null or (.bin != null and .bin != []))|.name' nimpkgs/*/*/*/meta.json|uniq
+    '';
   files.alias.save-stats =
     ''
       date -I                     > $PRJ_ROOT/../stats/last-update-at.txt
@@ -16,6 +20,7 @@
       pkgs-weights|cut -d, -f1,2  >> $PRJ_ROOT/../stats/dep-weights.csv
       pkgs-dots                   > $PRJ_ROOT/../stats/pkgs.dot
       pkgs-graph                  > $PRJ_ROOT/../stats/pkgs.mermaid
+      binary-list                 > $PRJ_ROOT/../stats/executables.csv
     '';
   files.text."/stats/README.md"  = 
     ''
@@ -23,13 +28,15 @@
 
       Here are some stats of those pkgs.
 
+      - [contributions-count.csv](./contributions-count.csv): count pkgs by owner
+      - [dep-weights-count.csv](./dep-weights-count.csv): count pkgs by weight
+      - [dep-weights.csv](./dep-weights.csv): sum all pkgs requirements
+      - [executables.csv](./executables.csv): list of executables pkgs
+      - [initials-count.csv](./initials-count.csv): count pkgs by initial letter
+      - [pkgs.dot](./pkgs.dot): graphviz dot file for future vizualizaitons
       - [versions-count.csv](./versions-count.csv): count versions by pkg
       - [versions-initials-count.csv](./versions-initials-count.csv): count versions by letter
-      - [dep-weights-count.csv](./dep-weights-count.csv): count pkgs by weight
-      - [initials-count.csv](./initials-count.csv): count pkgs by initial letter
-      - [contributions-count.csv](./contributions-count.csv): count pkgs by owner
-      - [dep-weights.csv](./dep-weights.csv): sum all pkgs requirements
-      - [pkgs.dot](./pkgs.dot): graphviz dot file for future vizualizaitons
+      
 
       Last update at ${builtins.readFile ../stats/last-update-at.txt}
     '';
--- stats/README.md
+++ stats/README.md
@@ -2,13 +2,15 @@
 
 Here are some stats of those pkgs.
 
+- [contributions-count.csv](./contributions-count.csv): count pkgs by owner
+- [dep-weights-count.csv](./dep-weights-count.csv): count pkgs by weight
+- [dep-weights.csv](./dep-weights.csv): sum all pkgs requirements
+- [executables.csv](./executables.csv): list of executables pkgs
+- [initials-count.csv](./initials-count.csv): count pkgs by initial letter
+- [pkgs.dot](./pkgs.dot): graphviz dot file for future vizualizaitons
 - [versions-count.csv](./versions-count.csv): count versions by pkg
 - [versions-initials-count.csv](./versions-initials-count.csv): count versions by letter
-- [dep-weights-count.csv](./dep-weights-count.csv): count pkgs by weight
-- [initials-count.csv](./initials-count.csv): count pkgs by initial letter
-- [contributions-count.csv](./contributions-count.csv): count pkgs by owner
-- [dep-weights.csv](./dep-weights.csv): sum all pkgs requirements
-- [pkgs.dot](./pkgs.dot): graphviz dot file for future vizualizaitons
+
 
 Last update at 2022-05-07
 
