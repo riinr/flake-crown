a1f6a375115699ae5133b2599f195baedce14445
hugosenari <hugosenari@gmail.com>
2022-06-16 00:26:17 -0300

    chore: fixing git index crash

--- refresher/flake.nix
+++ refresher/flake.nix
@@ -1,16 +1,17 @@
 {
   description          = "Nimble packages updater";
   inputs.dsf.url       = "github:cruel-intentions/devshell-files";
+  inputs.nimblesrc.url = "github:nim-lang/nimble";
 
-  outputs = { self, dsf }: dsf.lib.mkShell [
+  outputs = { self, dsf, nimblesrc }: dsf.lib.mkShell [
     ./project.nix
     { 
-      # env = [ 
-      #   { 
-      #     name  = "NIMBLESRC_PATH";
-      #     value = "${nimblesrc.defaultPackage.x86_64-linux}";
-      #   }
-      # ];
+      env = [ 
+        { 
+          name  = "NIMBLESRC_PATH";
+          value = "${nimblesrc.defaultPackage.x86_64-linux}";
+        }
+      ];
     }
   ];
 }
