6160ff25b74d774a9eba08d279d20d8f8874bdd1
hugosenari <hugosenari@gmail.com>
2022-12-27 05:16:00 -0300

    feat (pkgsRev): opencl template

--- pkgsRev/templates/opencl/flake.nix
+++ pkgsRev/templates/opencl/flake.nix
@@ -1,27 +1,28 @@
 {
-  inputs.nimrevs.url     = "github:riinr/flake-crown?dir=pkgsRev";
+  inputs.nimrevs.url     = "../../";
   inputs.nixpkgs2205.url = "github:NixOS/nixpkgs/22.05";
 
   outputs = inputs:
   let mkPkg  = nixpkgsVersion:
     let
       pkgs   = nixpkgsVersion.legacyPackages.x86_64-linux;
-      toFlag = builtins.map (src: "-p:${src}");
-      deps   = inputs.nimrevs.lib.srcs {
+      toFlag = builtins.map (dep: "-p:${dep.src}/src");
+      deps   = inputs.nimrevs.lib.resolve {
         inherit pkgs;
-        nimPkgs = [ "cligen" ];
+        nimPkgs = [ "opencl" ];
       };
     in
     pkgs.nimPackages.buildNimPackage {
+      buildInputs = map (dep: dep.pkg) (builtins.attrValues deps);
       nimBinOnly  = true;
-      nimFlags    = toFlag deps;
-      pname       = "cligen-example";
+      nimFlags    = toFlag (builtins.attrValues deps);
+      pname       = "opencl-example";
       src         = ./.;
       version     = "0.1.0";
       meta.ref     = "master";
       meta.version = "0.1.0";
-      meta.name    = "cligen-example";
-      meta.desc    = "Example of cligen";
+      meta.name    = "opencl-example";
+      meta.desc    = "Example of OpenCL";
     };
   in
   {
